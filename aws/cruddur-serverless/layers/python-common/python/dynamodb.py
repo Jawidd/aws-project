35833467b384e078b7e3d49da77d01c57e5b768c
Jawidd <jawid00786@gmail.come>
2025-12-22 10:17:41 +0000

    feat: SAM BUILD PROBLEM NOT FIXED(sam deployed without requirements)

--- aws/cruddur-serverless/layers/python-common/python/dynamodb.py
+++
@@ -1,6 +0,0 @@
-"""Shared DynamoDB utilities (placeholder)."""
-
-
-def get_client(*args, **kwargs):
-    # TODO: implement shared DynamoDB client helpers
-    return None
--- aws/cruddur-serverless/layers/python-common/python/logging.py
+++
@@ -1,6 +0,0 @@
-"""Shared logging utilities (placeholder)."""
-
-
-def setup_logger(*args, **kwargs):
-    # TODO: implement shared logging configuration
-    return None
