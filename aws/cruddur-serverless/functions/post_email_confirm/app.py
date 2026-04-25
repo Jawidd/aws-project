76a1775bab8cf4b47190a8f3d9f5ab3581caef00
Jawidd <jawid00786@gmail.com>
2026-04-25 19:01:13 +0100

    fix: resolve database URL from SSM parameter if ARN is provided

--- aws/cruddur-serverless/functions/post_email_confirm/app.py
+++ aws/cruddur-serverless/functions/post_email_confirm/app.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import boto3
 import psycopg2
 
 logger = logging.getLogger()
@@ -19,7 +20,14 @@ def lambda_handler(event, context):
 
         handle = preferred_username or (email.split("@")[0] if email else "unknown")
 
-        conn = psycopg2.connect(os.environ["DATABASE_URL"])
+        db_url = os.environ["DATABASE_URL"]
+        # If the value is an SSM ARN, resolve it
+        if db_url.startswith("arn:aws:ssm:"):
+            param_name = db_url.split(":")[-1]
+            ssm = boto3.client("ssm", region_name=os.environ.get("AWS_REGION", "eu-west-2"))
+            db_url = ssm.get_parameter(Name=param_name, WithDecryption=True)["Parameter"]["Value"]
+
+        conn = psycopg2.connect(db_url)
         with conn.cursor() as cur:
             cur.execute("""
                 INSERT INTO public.users (
